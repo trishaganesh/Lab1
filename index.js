45348964293a8a4ccc77461267295939a343e568
Trisha :) <trigan141@fusdk12.net>
2020-05-31 03:11:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let person1 = "Peter"
+let person2 = "Withnail"
+
+console.log( 'Hi, how are you, ' + person2 + '?' )
+console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
+
+
+for ( let n = 0; n < 10; n++ ) {
+  console.log( n );
+}
